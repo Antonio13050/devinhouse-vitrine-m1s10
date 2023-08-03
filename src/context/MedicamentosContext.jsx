dca76f525d0e56088cec27ace1456a1cd76841a2
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-08-03 12:09:50 -0700

    Ex 4 - Criação de contexto para armazenar Lista de Medicamentos

--- src/context/MedicamentosContext.jsx
+++ src/context/MedicamentosContext.jsx
@@ -1,13 +1,31 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const MedicamentosContext = createContext();
 
 export const MedicamentosContextProvider = ({ children }) => {
+    const [listaMedicamentos, setListaMedicamentos] = useState(
+        JSON.parse(localStorage.getItem("listaMedicamentos")) || []
+    );
+
     const adicionarMedicamento = () => {
         console.log("adicionar");
+        localStorage.setItem(
+            "listaMedicamentos",
+            JSON.stringify(listaMedicamentos)
+        );
+    };
+
+    const favoritarMedicamento = () => {
+        console.log("favoritar");
     };
     return (
-        <MedicamentosContext.Provider value={{ adicionarMedicamento }}>
+        <MedicamentosContext.Provider
+            value={{
+                listaMedicamentos,
+                adicionarMedicamento,
+                favoritarMedicamento,
+            }}
+        >
             {children}
         </MedicamentosContext.Provider>
     );
